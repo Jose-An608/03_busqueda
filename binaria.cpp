b2d1628fac9ebbfe33277c99c47a27f32831eb21
Jose <jvilcanquic@unjbg.edu.pe>
2024-09-29 22:35:09 -0500

    Se modifico solo para datos ordenados

--- binaria.cpp
+++ binaria.cpp
@@ -4,57 +4,34 @@
 using namespace std;
 
 int datosOrdenados(int [], int, int);
-//int datosDesordenados(int [], int, int);
 
 int main(){
-	int m=8, n, dato, p, q;
+	int m=8, dato, p;
 	int arr[] = {5,6,7,8,9,10,11,12};
-	int arr2[] = {8,4,222,11,0,7,5,4,3};
 		
-	cout << "Arreglo 1 (ordenado): " << endl;
+	cout << "Arreglo (ordenado): " << endl;
 	for(int i = 0; i<8; i++){
 		cout << arr[i] << " ";
 	}
 	
 	cout << endl << endl;
-	
-	cout << "Arreglo 2 (desordenado): " << endl;
-	for(int i = 0; i<8; i++){
-		cout << arr2[i] << " ";
-	}
-	
-	cout << endl << endl;
-	
-	cout << "Menu: " << endl;
-	cout << "1. Busqueda para datos ordenados " << endl;
-	cout << "2. Busqueda para datos desordenados " << endl;
-	cin >> n;
-	
-	cout << endl;
-	
+		
 	cout << "Digite el numero que quiere buscar: " << endl;
 	cin >> dato;
 	
-	
 	cout << endl;
 	
-	switch(n){
-		
-		case 1:
-			p = datosOrdenados(arr, m, dato);
-			 if (p >= 0){
-			 
-                cout << "El numero se encuentra en la posicion " << p+1 << " del primer arreglo. " << endl;
-            }else{
-			
-                cout << "El numero no se encuentra en el arreglo." << endl;
-			}
-            
-			break;
+	p = datosOrdenados(arr, m, dato);
+	
+	if (p >= 0){
+		cout << "El numero se encuentra en la posicion " << p+1 << " del primer arreglo. " << endl;
+    }else{
+		cout << "El numero no se encuentra en el arreglo." << endl;
 	}
-
+            
 	return 0;
 }
+
 int datosOrdenados(int arr[], int m , int dato){
 	int izq, der, cen, pos,n;
 	izq = 0;
@@ -80,4 +57,5 @@ int datosOrdenados(int arr[], int m , int dato){
 		return -1;
 	}
 }
+
 
