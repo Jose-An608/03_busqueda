06bf4293621f569c75e55f69117457a6efdc1619
Jose <jvilcanquic@unjbg.edu.pe>
2024-09-29 20:40:35 -0500

    Se realizo algunas modificaciones y se implemento la funcionalidad de busqueda para datos desordenados

--- secuencial.cpp
+++ secuencial.cpp
@@ -8,18 +8,27 @@ int main(){
 	int m=8, n, dato, p, q;
 	int arr[] = {1,2,3,4,5,6,7,8};
 	int arr2[] = {4,2,1,5,3,2,6,9};
+		
+	cout << "Arreglo 1 (ordenado): " << endl;
+	for(int i = 0; i<8; i++){
+		cout << arr[i] << " ";
+	}
 	
 	cout << endl << endl;
 	
-	cout << "Arreglo 1: " << endl;
+	cout << "Arreglo 2 (desordenado): " << endl;
 	for(int i = 0; i<8; i++){
-		cout << arr[i];
+		cout << arr2[i] << " ";
 	}
 	
-	cout << "Arreglo 2: " << endl;
-	for(int i = 0; i<8; i++){
-		cout << arr[i];
-	}
+	cout << endl << endl;
+	
+	cout << "Menu: " << endl;
+	cout << "1. Busqueda para datos ordenados " << endl;
+	cout << "2. Busqueda para datos desordenados " << endl;
+	cin >> n;
+	
+	cout << endl;
 	
 	cout << "Digite el numero que quiere buscar: " << endl;
 	cin >> dato;
@@ -27,24 +36,32 @@ int main(){
 	
 	cout << endl;
 	
-	cout << "Menu: " << endl;
-	cout << "1. Busqueda para datos ordenados " << endl;
-	cout << "2. Busqueda para datos desordenados " << endl;
-	cin >> n;
-	
-	cout << "El numero se encuentra en la posicion: " << endl;
-
 	switch(n){
 		
 		case 1:
 			p = datosOrdenados(arr, m, dato);
-			cout << p+1 << endl;
-		/*case 2: 
+			 if (p >= 0){
+			 
+                cout << "El numero se encuentra en la posicion " << p+1 << " del primer arreglo. " << endl;
+            }else{
+			
+                cout << "El numero no se encuentra en el arreglo." << endl;
+			}
+            
+			break;
+		
+		case 2: 
 			q = datosDesordenados(arr2,m, dato);
-			cout << q+1<< endl;
+			if(q>=0){
+				cout << "El numero se encuentra en la posicion " << q+1 << " del segundo arreglo. " << endl;
+			}else{
+				cout << "El numero no se encuentra en el arreglo. " << endl;
+				
+			}
+			break;
+		
 		default:
 			cout << "Saliendo..." << endl;
-			*/
 	}
 
 	return 0;
@@ -58,14 +75,12 @@ int datosOrdenados(int arr[], int m , int dato){
 	}
 	
 	if ((i>m-1) || (arr[i] > dato)){
-		pos = -i;
+		return pos = -i;
 
 	}else{
-		pos = i;
+		return pos = i;
 
 	}
-	
-	return pos;
 }
 
 int datosDesordenados(int arr2[], int m, int dato){
@@ -76,8 +91,9 @@ int datosDesordenados(int arr2[], int m, int dato){
 	}
 	
 	if (i<=m){
-		pos = i;
+		return pos = i;
+	}else{
+		return pos = -1;
 	}
 	
-	return pos;
 }
